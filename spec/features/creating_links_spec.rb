a0888df8a45f936c88490129fb4b8be7e4f011f9
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-11-04 12:34:03 +0000

    Challenge 11 creating a form to input links

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,12 @@
+feature 'Creating new links' do
+  scenario 'fill in form' do
+    visit('/links/new')
+    fill_in 'title', with: 'Amazon'
+    fill_in 'url', with: 'www.amazon.co.uk'
+    click_button('Submit')
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Amazon')
+    end
+  end
+end
