e23152d51b090f8f73d8ef7fe3b1033c2d8fda5b
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-11-04 11:58:03 +0000

    Challenge 10: created a link in the model

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,6 +6,7 @@ class Link
   property :title, String
   property :url, String
 end
+DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default,'postgres://localhost/bookmark_manager_test')
 DataMapper.finalize
 DataMapper.auto_upgrade!
